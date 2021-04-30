2da738ca4fdb152fbd4d1cc777ab23a3806d6252
VerdiFran <tmw.kva@yandex.ru>
2021-04-30 15:12:13 +0700

    Add basic layout, routes and style variables

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import './App.css'
+import {Layout} from 'antd'
+import 'antd/dist/antd.css'
+import Logo from './components/Logo/Logo'
+import React from 'react'
+import {BrowserRouter, Switch, Route, Redirect, withRouter} from 'react-router-dom'
+import {TO_HOME, TO_REGISTRATION} from './routes'
+import Home from './components/Home/Home'
+import Registration from './components/Registration/Registration'
 
-function App() {
+const App = () => {
+    const {Header, Content, Footer} = Layout
+
     return (
         <div className="App">
-
+            <Layout className="layout">
+                <Header>
+                    <Logo/>
+                </Header>
+                <Content>
+                    <Switch>
+                        <Route exact path={TO_HOME} render={() => <Home/>}/>
+                        <Route path={TO_REGISTRATION} render={() => <Registration/>}/>
+                        <Redirect to={TO_HOME}/>
+                    </Switch>
+                </Content>
+                <Footer>Rooming ©2021</Footer>
+            </Layout>
         </div>
     )
 }
 
-export default App
+const AppContainer = withRouter(App)
+
+/**
+ * Main app, container for app
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const MainApp = () => {
+    return (
+        <BrowserRouter>
+            <AppContainer/>
+        </BrowserRouter>
+    )
+}
+
+export default MainApp
