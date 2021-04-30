ed65aec72e68b0ad5f376e2f6914dd1ec1a60e43
VerdiFran <tmw.kva@yandex.ru>
2021-04-30 15:11:11 +0700

    Add home page

---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+/**
+ * Home component with information about our project
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const Home = () => {
+    return (
+        <div>
+            Home
+        </div>
+    )
+}
+
+export default Home
