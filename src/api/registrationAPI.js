38e8ca754226996ea37a571b6c16d46e64634259
VerdiFran <tmw.kva@yandex.ru>
2021-05-01 13:34:09 +0700

    Add registration API for adding customer (company)

---
+++ src/api/registrationAPI.js
@@ -0,0 +1,16 @@
+import * as axios from 'axios'
+
+const instance = axios.create({
+    baseURL: 'http://176.119.158.143/api'
+})
+
+export const registrationAPI = {
+    /**
+     * POST request for adding new company
+     * @param {{name: string, email: string, contactPhone: string, employees: [{firstName: string, lastName: string, email: string, phoneNumber: string}]}} company Information about company
+     * @returns {*}
+     */
+    addCompany(company) {
+        return instance.post('add-requests/companies', company)
+    }
+}
